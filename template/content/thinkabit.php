b3976f4dbc780de6a8a52031fcf85001a9e874c1
EmersonLucena <emersonleonardolucena@hotmail.com>
2015-12-11 20:44:43 -0300

    sobre nos

--- template/content/thinkabit.php
+++ template/content/thinkabit.php
@@ -1,107 +1,106 @@
-			<div id="content" class="team-page">
-				<section id="first-section">
-					<div class="container-fluid">
-						<div class="section-meta">
-							<div class="row">
-								<div class="col-xs-8 col-xs-offset-2">
-									<h2 class="section-title">Conheça a Think a bit</h2>
-									<h3 class="section-description">A Think a bit é uma nova empresa de desenvolvimento web, mobile e sistemas empresariais que tem em vista a criação de aplicações intuitivas com um design moderno, segurança e confiabilidade.</h3>
-								</div>
-							</div>
-						</div>
-						<div class="row">
-							<div class="col-lg-12">
-								<div class="row">
-									<div class="col-lg-10 col-sm-10 col-sm-offset-1">
-										<div class="col-lg-10 col-xs-12 col-lg-offset-1">
-											<div class="col-sm-4">
-												<div class="feature">
-													<div class="circular">
-														<div class="team-member" id="emerson"></div>
-													</div>
-													<div class="feature-meta">
-														<h4>Emerson Lucena</h4>
-														<p>Feature description</p>
-													</div>
-												</div>
-											</div>
-											<div class="col-sm-4">
-												<div class="feature">
-													<div class="circular">
-														<div class="team-member" id="francisco"></div>
-													</div>
-													<div class="feature-meta">
-														<h4>Francisco Júnior</h4>
-														<p>Feature description</p>
-													</div>
-												</div>
-											</div>
-											<div class="col-sm-4">
-												<div class="feature">
-													<div class="circular">
-														<div class="team-member" id="joao"></div>
-													</div>
-													<div class="feature-meta">
-														<h4>João Maurício</h4>
-														<p>Feature description</p>
-													</div>
-												</div>
-											</div>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
+<div id="content" class="team-page">
+	<section id="first-section">
+		<div class="container-fluid">
+			<div class="section-meta">
+				<div class="row">
+					<div class="col-xs-8 col-xs-offset-2">
+						<h2 class="section-title">Conheça a Think a bit</h2>
+						<h3 class="section-description">A Think a bit é uma nova empresa de desenvolvimento web, mobile e sistemas empresariais que tem em vista a criação de aplicações intuitivas com um design moderno, segurança e confiabilidade.</h3>
 					</div>
-				</section>
-				<section>
-					<div class="container-fluid">
-						<div class="section-meta">
-							<h2 class="section-title">Dúvidas?</h2>
-							<div class="row">
-								<div class="col-xs-10 col-xs-offset-1">
-									<div class="row">
-										<div class="col-xs-12 col-md-6 duvida">
-											<h3 class="section-subtitle">O que é a Think a bit?</h3>
-											<p class="section-description">A Think a bit é uma empresa de desenvolvimento em geral que tem como foco a criação de aplicações simples e intuitivas para facilitar o dia a dia de nossos clientes.</p>
-										</div>
-										<div class="col-xs-12 col-md-6 duvida">
-											<h3 class="section-subtitle">Por que devo contratar vocês?</h3>
-											<p class="section-description">A Think a bit é uma empresa de desenvolvimento em geral que tem como foco a criação de aplicações simples e intuitivas para facilitar o dia a dia de nossos clientes</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-xs-10 col-xs-offset-1">
-									<div class="row">
-										<div class="col-xs-12 col-md-6 duvida">
-											<h3 class="section-subtitle">Rick Astley</h3>
-											<p class="section-description">We're no strangers to love<br />You know the rules and so do I<br />A full commitments what I´m thinking of<br />You wouldn´t get this from any other guy<br />I just wanna tell you how I'm feeling<br />Gotta make you understand</p>
-										</div>
-										<div class="col-xs-12 col-md-6 duvida">
-											<h3 class="section-subtitle">Never gonna give you up</h3>
-											<p class="section-description">Never gonna let you down<br />Never gonna run around<br />And desert you<br />Never gonna make you cry<br />Never gonna say goodbye<br />Never gonna tell a lie<br />And hurt you</p>
-										</div>
-									</div>
-								</div>
-							</div>
-							<div class="row">
-								<div class="col-xs-10 col-xs-offset-1">
-									<div class="row">
-										<div class="col-xs-12 col-md-6 duvida">
-											<h3 class="section-subtitle">Marciano não quer mais tomar cerveja...</h3>
-											<p class="section-description">Só uísque e energético na night<br />Marciano não quer treta na quebrada<br />Cola só com as gostosa só dona fight<br />Marciano não quer mais usar os revolver<br />Nem guerria por causa da cocaína<br />Marciano não quer mais saber de nada<br />Só quer no dia do frevo tá de cima</p>
-										</div>
-										<div class="col-xs-12 col-md-6 duvida">
-											<h3 class="section-subtitle">https://youtu.be/r81mQ66K2OI</h3>
-											<p class="section-description">Fumaça<br />Fumaça<br />Só cão de raça<br />
-	Fumaça na quadra</p>
-										</div>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</section>
+				</div>
 			</div>
+			<div class="row">
+				<div class="col-lg-12">
+					<div class="row">
+						<div class="col-lg-10 col-sm-10 col-sm-offset-1">
+							<div class="col-lg-10 col-xs-12 col-lg-offset-1">
+								<div class="col-sm-4">
+									<div class="feature">
+										<div class="circular">
+											<div class="team-member" id="emerson"></div>
+										</div>
+										<div class="feature-meta">
+											<h4>Emerson Lucena</h4>
+											<p>Feature description</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<div class="feature">
+										<div class="circular">
+											<div class="team-member" id="francisco"></div>
+										</div>
+										<div class="feature-meta">
+											<h4>Francisco Júnior</h4>
+											<p>Feature description</p>
+										</div>
+									</div>
+								</div>
+								<div class="col-sm-4">
+									<div class="feature">
+										<div class="circular">
+											<div class="team-member" id="joao"></div>
+										</div>
+										<div class="feature-meta">
+											<h4>João Maurício</h4>
+											<p>Feature description</p>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section>
+		<div class="container-fluid">
+			<div class="section-meta">
+				<h2 class="section-title">Dúvidas?</h2>
+				<div class="row">
+					<div class="col-xs-10 col-xs-offset-1">
+						<div class="row">
+							<div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-0 col-lg-5 col-lg-offset-1 duvida">
+								<h3 class="section-subtitle">O que é a Think a bit?</h3>
+								<p class="section-description">A Think a bit é uma empresa de desenvolvimento em geral que tem como foco a criação de aplicações simples e intuitivas para facilitar o dia a dia de nossos clientes.</p>
+							</div>
+							<div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-0 col-lg-5 duvida">
+								<h3 class="section-subtitle">Por que devo contratar vocês?</h3>
+								<p class="section-description">setneilc sosson ed aid a aid o ratilicaf arap savitiutni e selpmis seõçacilpa ed oãçairc a ocof omoc met euq lareg me otnemivlovnesed ed aserpme amu é tib a knihT A</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-10 col-xs-offset-1">
+						<div class="row">
+							<div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-0 col-lg-5 col-lg-offset-1 duvida">
+								<h3 class="section-subtitle">Quais serviços a Think a bit abrange?</h3>
+								<p class="section-description">setneilc sosson ed aid a aid o ratilicaf arap savitiutni e selpmis seõçacilpa ed oãçairc a ocof omoc met euq lareg me otnemivlovnesed ed aserpme amu é tib a knihT A</p>
+							</div>
+							<div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-0 col-lg-5 duvida">
+								<h3 class="section-subtitle">P A C O T E S</h3>
+								<p class="section-description">setneilc sosson ed aid a aid o ratilicaf arap savitiutni e selpmis seõçacilpa ed oãçairc a ocof omoc met euq lareg me otnemivlovnesed ed aserpme amu é tib a knihT A</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-10 col-xs-offset-1">
+						<div class="row">
+							<div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-0 col-lg-5 col-lg-offset-1 duvida">
+								<h3 class="section-subtitle">A T U A L I Z A R</h3>
+								<p class="section-description">setneilc sosson ed aid a aid o ratilicaf arap savitiutni e selpmis seõçacilpa ed oãçairc a ocof omoc met euq lareg me otnemivlovnesed ed aserpme amu é tib a knihT A</p>
+							</div>
+							<div class="col-xs-10 col-xs-offset-1 col-md-6 col-md-offset-0 col-lg-5 duvida">
+								<h3 class="section-subtitle">C U S T O</h3>
+								<p class="section-description">setneilc sosson ed aid a aid o ratilicaf arap savitiutni e selpmis seõçacilpa ed oãçairc a ocof omoc met euq lareg me otnemivlovnesed ed aserpme amu é tib a knihT A</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+</div>
