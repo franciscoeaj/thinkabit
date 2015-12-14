b0c2175c2b7c8c1602eba0ace0c2e81317f06d3f
joaomcarvalho <joao_carvalho1997@hotmail.com>
2015-12-14 13:58:15 -0300

    Mudanças na index

--- template/content/index.php
+++ template/content/index.php
@@ -1,12 +1,84 @@
-			<div id="content"> 
+			<div id="content" class="index-page"> 
 				<section id="first-section">
 					<div class="container-fluid">
-							<div class="section-meta">
-								<div class="row">
-										<h2 class="section-title">Simples, confiável e robusto</h2>
-										<h3 class="section-description">Além da comodidade de aplicações simples e intuitivas, seu sistema estará em um ambiente completamente seguro que possui garantia de total estabilidade, e você ainda conta com suporte total 24 horas, disponível 365 dias por ano</h3>
+						<div class="section-meta">
+							<div class="row">
+								<h2 class="section-title">Simples, confiável e robusto</h2>
+								<h3 class="section-description">Além da comodidade de aplicações simples e intuitivas, seu sistema estará em um ambiente completamente seguro que possui garantia de total estabilidade, e você ainda conta com suporte total 24 horas, disponível 365 dias por ano</h3>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-lg-12 col-sm-12 col-lg-offset-1 col-sm-offset-1">
+								<div class="col-lg-12 col-xs-12">
+									<div class="col-lg-2 col-sm-2">
+										<div id="visual-identity" class="feature service">
+											<div class="circular">
+												<div class="service-image"></div>
+											</div>
+											<div class="feature-meta">
+												<h4>Preço diferenciado</h4>
+												<p>Possuimos pacotes com valores que se adequam aos diferentes perfis dos clientes, visando atender bem a todos</p>
+											</div>
+										</div>
+									</div>
+									<div class="col-lg-2 col-sm-2">
+										<div id="security" class="security">
+											<div class="circular">
+												<div class="service-image"></div>
+											</div>
+											<div class="feature-meta">
+												<h4>Segurança</h4>
+												<p>A segurança do seu sistema é fundamental para nós, por isso possuímos servidores robustos com softwares de tecnologia mais recente possível</p>
+											</div>
+										</div>
+									</div>
+									<div class="col-lg-2 col-sm-2">
+										<div id="suporte" class="suporte">
+											<div class="circular">
+												<div class="service-image"></div>
+											</div>
+											<div class="feature-meta">
+												<h4>Suporte 24h</h4>
+												<p>Através do nosso helpdesk ou de qualquer rede social, estamos disponíveis 24 horas por dia para lhe atender</p>
+											</div>
+										</div>
+									</div>
+									<div class="col-lg-2 col-sm-2">
+										<div id="clock" class="clock">
+											<div class="circular">
+												<div class="service-image"></div>
+											</div>
+											<div class="feature-meta">
+												<h4>Tempo recorde</h4>
+												<p>Como amamos nosso trabalho, o resultado é uma aplicação com todos os recursos já citados e entregue rapidamente</p>
+											</div>
+										</div>
+									</div>
+									<div class="col-lg-2 col-sm-2">
+										<div id="browser" class="browser">
+											<div class="circular">
+												<div class="feature-meta"></div>
+											</div>
+											<div class="feature-meta">
+												<h4>Fácil gerenciamento</h4>
+												<p>Todo e qualquer tipo de software desenvolvido por nós possui uma simples e intuitva interface na qual você pode configurá-lo como melhor precisar</p>
+											</div>
+										</div>
 									</div>
 								</div>
 							</div>
+						</div>
+					</div>
 				</section>
-			</div>
+			    <section>
+					<div class="container-fluid">
+						<div class="section-meta">
+							<h2 class="section-title">Portfolio</h2>
+							<h3 class="section-description">Apenas uma pequena prévia dos nossos últimos serviços realizados</h3>
+							<div class="row">
+								
+									</div>
+								</div>
+							</div>
+					</section>
+			   </div>       <!-- visual-identy feature serivce -->
