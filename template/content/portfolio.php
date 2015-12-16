f74cfdfb3538e71848676342e28a5060588100c1
EmersonLucena <emersonleonardolucena@hotmail.com>
2015-12-16 16:13:34 -0300

    portfolio

--- template/content/portfolio.php
+++ template/content/portfolio.php
@@ -3,7 +3,64 @@
 					<div class="container-fluid">
 						<div class="section-meta">
 							<h2 class="section-title">Criados com um carinho especial</h2>
-							<h3 class="section-description">Como fazemos o que gostamos, o resultado é sempre um produto bonito, incriável e único!</h3>
+							<h3 class="section-description">Como gostamos do que fazemos, o resultado é sempre um produto bonito, incriável e único!</h3>
+							<div class="row">
+								<div class="col-sm-12 col-xs-12">
+									<div class="col-sm-2"></div>
+									<div class="col-sm-2">
+										<div id="p1" class="portfolio service">
+											<div class="circular">
+												<div class="portfolio-image">
+													<div class="portfolio-meta">
+														<p class="type">Layout</p>
+														<p class="client">Site.com</p>
+														<a class="more">+</a>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-2">
+										<div id="p2" class="portfolio service">
+											<div class="circular">
+												<div class="portfolio-image">
+													<div class="portfolio-meta">
+														<p class="type">Layout</p>
+														<p class="client">Site.com</p>
+														<a class="more">+</a>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-2">
+										<div id="p3" class="portfolio service">
+											<div class="circular">
+												<div class="portfolio-image">
+													<div class="portfolio-meta">
+														<p class="type">Empatech</p>
+														<p class="client">empatech.com.br</p>
+														<a class="more">+</a>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+									<div class="col-sm-2">
+										<div id="p4" class="portfolio service">
+											<div class="circular">
+												<div class="portfolio-image">
+													<div class="portfolio-meta">
+														<p class="type">Layout</p>
+														<p class="client">Site.com</p>
+														<a class="more">+</a>
+													</div>
+												</div>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
 						</div>
 					</div>
 				</section>
